69aa11cfc9c3ae830942d7a9b8dea89b7baf06bb
Ilya Zhurba <i.n.zhurba@gmail.com>
2020-07-26 13:39:38 -0600

    Added stats to base enemy constructor, need to figure out stats/leveling

---
+++ js/enemy.js
@@ -0,0 +1,20 @@
+class Enemy {
+	constructor(name, level) {
+		this.name = name;
+		this.level = level;
+		this.health = 50;
+		this.strength = 10;
+		this.intelligence = 10;
+		this.agility = 5;
+		this.stamina = 20;
+		this.mana = 20;
+		this.physicalDefense = 10;
+		this.magicalDefense = 10;
+		this.leftWeapon;
+		this.rightWeapon;
+		this.rightRing;
+		this.leftRing;
+		this.head;
+		this.body;
+		this.legs;
+		
