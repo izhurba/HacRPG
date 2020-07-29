b574a4b7db6cde23b86a6c2e3b192c4e73f6b6dd
Ilya Zhurba <i.n.zhurba@gmail.com>
2020-07-29 16:05:37 -0600

    Added start page, with css and placeholder images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-function startGame() {}
-startGame()
+function startGame() {
+	var x = document.getElementById("startScreen");
+ 	x.style.display = "none";
+	var y = document.getElementById("mainScreen");
+	y.style.display = "block";
+	var f = document.getElementByTagName("footer");
+	f.style.display = "block";
+}
+
+
 
