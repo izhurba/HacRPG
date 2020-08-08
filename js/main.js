68545363658bce2145d3671a3c33a40f15fdfd45
Ilya Zhurba <i.n.zhurba@gmail.com>
2020-08-07 22:34:01 -0600

    Added placeholder triangle with wasd control on canvas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 var player;
 var job;
 var enemy;
+var canvas;
+var ctx;
+var deltaX = 0;
+var deltaY = 0;
+
 
 function startGame() {
 	var x = document.getElementById("startScreen");
@@ -19,15 +24,16 @@ function startGame() {
 	health.value = 100 * (player.maxHealth / player.currentHealth);
 	var name = document.getElementById("playerName").innerHTML = player.name;
 	
+	canvas = document.getElementById("mapCanvas");
+	ctx = canvas.getContext("2d");
 	drawCanvas();
+	drawTri();
 
 }
 
+
+
 function drawCanvas() {
-	var canvas = document.getElementById("mapCanvas");
-	var ctx = canvas.getContext("2d");
-	//canvas.width = window.innerWidth - 200;
-	//canvas.height= window.innerHeight - 200;
 	canvas.width = window.innerWidth - 250;
 	canvas.height= window.innerHeight- 200;
 	ctx.fillStyle = "#777777";
@@ -36,6 +42,50 @@ function drawCanvas() {
 
 function redraw() {
 	//Redraw canvas on events
+}
+
+function drawTri() {
+	ctx.clearRect(0,0, canvas.width, canvas.height);
+
+	ctx.beginPath();
+	ctx.moveTo(200 + deltaX,100 + deltaY);
+	ctx.lineTo(170 + deltaX,150 + deltaY);
+	ctx.lineTo(230 + deltaX,150 + deltaY);
+	ctx.closePath();
+
+	ctx.lineWidth = 10;
+	ctx.strokeStyle = "rgba(102, 102, 102, 1)";
+	ctx.stroke();
+
+}
+
+
+window.addEventListener("keydown", characterControl, false);
+
+function characterControl(e) {
+	switch(e.keyCode) {
+		case 65:
+			//KeyA
+			deltaX -= 2;
+			break;
+
+		case 68:
+			//KeyD
+			deltaX += 2;
+			break;
+
+		case 87:
+			//KeyW
+			deltaY -= 2;
+			break;
+
+		case 83:
+			//KeyS
+			deltaY += 2;
+			break;
+	}
+
+	drawTri();
 }
 
 
